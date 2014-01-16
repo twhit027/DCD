830bdf87f78d2b4fc762c919818febde2992ab5e
cburks <cburkholder@gannett.com>
2014-01-16 09:00:32 -0600

    1/16 Updates

--- includes/constants.php
+++ includes/constants.php
@@ -1,10 +1,11 @@
-<?
-//define("DB_SERVER", "50.63.231.153");
-define("DB_SERVER", "127.0.0.1");
-define("DB_PORT", "3306");
-//define("DB_USER", "classifiedsproject");
-define("DB_USER", "classifiedsproj");
-define("DB_PASS", "Classdb13!");
-define("DB_NAME", "classifiedsproje");
-define("TBL_LISTING", "listing");
+<?php
+//define('DB_SERVER', '50.63.231.153');
+define('DB_SERVER', 'localhost');
+
+define('DB_PORT', '3306');
+//define('DB_USER', 'classifiedsproject');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'classifiedsproje');
+define('TBL_LISTING', 'listing');
 ?>
