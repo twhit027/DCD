99548d32bf4e50f667cfc6b0916679637692de5a
Jay Hicks <jhicks@gmti.gannett.com>
2014-02-24 16:57:45 -0500

    DCD-20 - made changes to the footer file to allow the addition hrefs requested

--- includes/footer.php
+++ includes/footer.php
@@ -3,12 +3,15 @@
 	$data .= '<a href="'.$siteUrl.'/news">News</a>&nbsp;|&nbsp;';
 	$data .= '<a href="'.$siteUrl.'/cars">Sports</a>&nbsp;|&nbsp;';
 	$data .= '<a href="'.$siteUrl.'/news/business">Business</a>&nbsp;|&nbsp;';
+	$data .= '<a href="'.$siteUrl.'/Entertainment">Entertainment</a>&nbsp;|&nbsp;';
 	$data .= '<a href="'.$siteUrl.'/life">Life</a>&nbsp;|&nbsp;';
+	$data .= '<a href="'.$siteUrl.'/Communities">Communities</a>&nbsp;|&nbsp;';
 	$data .= '<a href="'.$siteUrl.'/thingstodo">Things to Do</a>&nbsp;|&nbsp;';
 	$data .= '<a href="'.$siteUrl.'/opinion">Opinion</a>&nbsp;|&nbsp;';						
 	$data .= '<a href="http://www.legacy.com/obituaries/'.$siteName.'/">Obituaries</a>&nbsp;|&nbsp;';	
 	$data .= '<a href="'.$siteUrl.'/help">Help</a></p>';
-	$data .= '<p>Copyright &copy; 2014 '.$siteName.'. All rights reserved. Users of this site agree to the Terms of Service, ';
+	$data .= '<p>Copyright &copy; 2014 www.'.$siteName.'.com. All rights reserved. Users of this site agree to the ';
+	$data .= '<a href="'.$siteUrl.'/section/terms">Terms of Service</a>, ';
 	$data .= '<a href="'.$siteUrl.'/section/privacy">Privacy Notice</a>, and <a href="'.$siteUrl.'/section/privacy#adchoices">Ad Choices</a></p></div>';				
 			
 	echo $data;
