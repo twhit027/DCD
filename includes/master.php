b17405a442811a2c7733022d2c025cd3e6c6f6be
JayKHicks <jhicks@gmti.gannett.com>
2014-04-22 17:43:08 -0400

    DCD-203 - added changes that allowed both searches to work together

--- includes/master.php
+++ includes/master.php
@@ -80,12 +80,15 @@ if(isset($metadata))
         <nav role="navigation" class="collapse navbar-collapse bs-navbar-collapse side-navbar ">
         <div class="visible-xs">
         <h3 style="color:#3276B1;">Search Our Classifieds</h3>
-        <input id="fullTextBox" type="text" name="search" class="form-control" value="<?php echo $fullText; ?>">
-                <span class="input-group-btn">
-                    <a id="ftSearch" class="btn btn-primary" style="vertical-align: bottom;width:100%;margin-bottom:5px;"><img src="img/white-magnifying-glass-20.png"></a>
-                   
-                </span>
-                        
+            <div class="input-group">
+                <input id="fullTextBox1" type="text" name="search" class="form-control" value="<?php echo $fullText; ?>">
+                        <span class="input-group-btn">
+                            <button id="ftSearchbtn1" class="btn btn-primary" type="button">
+                                <img src="img/white-magnifying-glass-20.png">
+                            </button>
+                        </span>
+            </div>
+            <h3 style="color:#3276B1;">Or Select A Category</h3>
         <ul class="nav nav-list accordion" id="sidenav-accordian" style="padding-bottom:10px;">
         <?php echo $nav->getSideNavigation($app->getCategories()) ?>
         </ul></div></nav>
@@ -122,9 +125,9 @@ else if($device =="phone")
                 <div role="navigation" id="sidebar" style="background-color:#000; padding-left:15px; padding-right:15px; padding-top:5px">
                     <h3 style="color:#3276B1;">Search Our Classifieds</h3>
                     <div class="input-group">
-                        <input id="fullTextBox" type="text" name="search" class="form-control" value="<?php echo $fullText; ?>">
+                        <input id="fullTextBox2" type="text" name="search" class="form-control" value="<?php echo $fullText; ?>">
                         <span class="input-group-btn">
-                            <button id="ftSearch" class="btn btn-primary" type="button">
+                            <button id="ftSearchbtn2" class="btn btn-primary" type="button">
                                 <img src="img/white-magnifying-glass-20.png">
                             </button>
                         </span>
@@ -172,9 +175,9 @@ else if($device == "tablet")
 <script src="3rdParty/jasny-bootstrap/js/jasny-bootstrap.min.js"></script>
 <script>
     $( document ).ready(function() {
-        $("#ftSearch").click(function(e) {
+        $("#ftSearchbtn1").click(function(e) {
             e.preventDefault();
-            ft = $("#fullTextBox").val().trim();
+            ft = $("#fullTextBox1").val().trim();
             place='';
             posit='';
             /*if ($('#place').val()) {
@@ -183,6 +186,20 @@ else if($device == "tablet")
             if ($('#place').val()) {
                 posit= $('#posit').val();
             }*/
+            window.location.href = 'category.php?place='+encodeURIComponent(place)+'&posit='+encodeURIComponent(posit)+'&ft='+encodeURIComponent(ft);
+        });
+
+        $("#ftSearchbtn2").click(function(e) {
+            e.preventDefault();
+            ft = $("#fullTextBox2").val().trim();
+            place='';
+            posit='';
+            /*if ($('#place').val()) {
+             place= $('#place').val();
+             }
+             if ($('#place').val()) {
+             posit= $('#posit').val();
+             }*/
             window.location.href = 'category.php?place='+encodeURIComponent(place)+'&posit='+encodeURIComponent(posit)+'&ft='+encodeURIComponent(ft);
         });
 
