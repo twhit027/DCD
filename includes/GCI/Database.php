89b11c0f76bc3d53a505e6648fdafcae662257ee
JayKHicks <jhicks@gmti.gannett.com>
2014-07-10 15:33:10 -0400

    DCD-267 - remove the KLogger file from the includes

--- includes/GCI/Database.php
+++ includes/GCI/Database.php
@@ -14,7 +14,7 @@ class Database extends \PDO
     private $con = false;
     private $log;
 
-    public function __construct()
+    public function __construct($logDir = '')
     {
         try {
             $this->connection_string = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';port=' . DB_PORT . ';charset=utf8';
