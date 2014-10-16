da36dde4738dafb4d840ea88e3d0645b4203c350
JayKHicks <jhicks@gmti.gannett.com>
2014-10-16 12:03:18 -0400

    DCD-312 - update to the latest jquery 1

--- includes/master.php
+++ includes/master.php
@@ -217,6 +217,7 @@ else if($device == "tablet")
 <?php echo $nav->getBottomNavigation($siteUrl, $palette, $siteName, $siteBottomData); ?>
 </footer>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
 <script>window.jQuery || document.write('<script src="3rdParty/jquery/jquery.min.js"><\/script>')</script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script>
