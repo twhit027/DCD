5f5bf3f0469c8f839f4bb7eb7e4bd0a0a118d8d9
JayKHicks <jhicks@gmti.gannett.com>
2015-05-27 15:40:21 -0400

    Add datbase column to control all links on landing page

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ $app->logInfo('Landin Page(FORWARDED_FOR: '.@$_SERVER['HTTP_X_FORWARDED_FOR'].',
 $siteName = $app->getSite()->getSiteName();
 $siteUrl = $app->getSite()->getSiteUrl();
 $busName = $app->getSite()->getBusName();
+$siteLinks = $app->getSite()->getSiteLinks();
 
 $metadata = '
 <title>'.$busName.' - Classifieds Landing Page</title>
@@ -23,26 +24,36 @@ $metadata = '
 <meta itemprop="name" content="Classifieds Landing page">
 <meta itemprop="description" content="Classifieds Landing page for '.$busName.'">';
 
-$thirdPartyCars = defined('THIRD_PARTY_CARS') ? THIRD_PARTY_CARS : true;
-$thirdPartyJobs = defined('THIRD_PARTY_JOBS') ? THIRD_PARTY_JOBS : true;
-$thirdPartyHomes = defined('THIRD_PARTY_HOME') ? THIRD_PARTY_HOME : true;
+$thirdPartyPlace = isset($siteLinks['place']) ? $siteLinks['place'] : 'http://'.$siteName.'.gannettclassifieds.com';
+$thirdPartyHelp = isset($siteLinks['help']) ? $siteLinks['help'] : $siteUrl.'/classifiedshelp';
+$thirdPartyCars = isset($siteLinks['cars']) ? $siteLinks['cars'] : $siteUrl.'/cars';
+$thirdPartyJobs = isset($siteLinks['jobs']) ? $siteLinks['jobs'] : $siteUrl.'/jobs';
+$thirdPartyHomes = isset($siteLinks['homes']) ? $siteLinks['homes'] : $siteUrl.'/homes';
 
-$cars = $jobs = $homes = $rows = '';
+$cars = $jobs = $homes = $linkRow = $placeRow = $helpRow = '';
 
 if ($thirdPartyCars) {
-    $cars = '<h4>Cars</h4><a href="'.$siteUrl.'/cars"><img alt="Cars.com" src="img/partners/130-cars.gif"></a><p><a class="button" href="'.$siteUrl.'/cars"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">View Autos</button></a></p>';
+    $cars = '<h4>Cars</h4><a href="'.$thirdPartyCars.'"><img alt="Cars.com" src="img/partners/130-cars.gif"></a><p><a class="button" href="'.$thirdPartyCars.'"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">View Autos</button></a></p>';
 }
 
 if ($thirdPartyJobs) {
-    $jobs = '<h4>Jobs</h4><a href="'.$siteUrl.'/jobs"><img alt="micareerbuilder.com" src="img/partners/130-careerbuilder.gif"></a><p><a class="button" href="'.$siteUrl.'/jobs"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">View Jobs</button></a></p>';
+    $jobs = '<h4>Jobs</h4><a href="'.$thirdPartyJobs.'"><img alt="careerbuilder.com" src="img/partners/130-careerbuilder.gif"></a><p><a class="button" href="'.$thirdPartyJobs.'"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">View Jobs</button></a></p>';
 }
 
 if ($thirdPartyHomes) {
-    $homes = '<h4>Homes</h4><a href="'.$siteUrl.'/homes"><img alt="homefinder.com" src="img/partners/130-homefinder.gif" ></a><p><a class="button" href="'.$siteUrl.'/homes"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">View Homes</button></a></p>';
+    $homes = '<h4>Homes</h4><a href="'.$thirdPartyHomes.'"><img alt="homefinder.com" src="img/partners/130-homefinder.gif" ></a><p><a class="button" href="'.$thirdPartyHomes.'"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">View Homes</button></a></p>';
 }
 
 if ($thirdPartyCars || $thirdPartyJobs || $thirdPartyHomes) {
-    $rows = '<h1>Featured Partner Classified Services</h1><div class="row"><div class="col-md-4">'.$cars.'</div><div class="col-md-4">'.$jobs.'</div><div class="col-md-4">'.$homes.'</div></div>';
+    $linkRow = '<h1>Featured Partner Classified Services</h1><div class="row"><div class="col-md-4">'.$cars.'</div><div class="col-md-4">'.$jobs.'</div><div class="col-md-4">'.$homes.'</div></div>';
+}
+
+if ($thirdPartyPlace) {
+    $placeRow = '<p><a class="button" href="'.$thirdPartyPlace.'"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">Place an Ad</button></a></p>';
+}
+
+if ($thirdPartyHelp) {
+    $helpRow = '<p><a class="button" href="'.$thirdPartyHelp.'" target="_blank"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">Classifieds Help</button></a></p>';
 }
 
 $mainContent = <<<EOS
@@ -53,9 +64,9 @@ $mainContent = <<<EOS
 <p>In just a few clicks, you can place your ads online, in print or both. And with improved ad displays, your ad is sure to get noticed!</p>
 <p>From vehicles to pets to garage sales to services, we provide the most effective ways to sell to potential local
 buyers through our leading mobile, online and print solutions.</p>
-<p><a class="button" href="http://$siteName.gannettclassifieds.com"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">Place an Ad</button></a></p>
-<p><a class="button" href="http://$siteName.com/classifiedshelp" target="_blank"><button type="button" class="btn btn-primary btn-lg" style="width:100%;">Classifieds Help</button></a></p>
-$rows
+$placeRow
+$helpRow
+$linkRow
 EOS;
 
 include("../includes/master.php");
